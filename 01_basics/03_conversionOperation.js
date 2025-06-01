f5a1c43d3331c1f098e5823bbb311104af533b21
yschamp <166626158+yschamp@users.noreply.github.com>
2025-06-01 10:51:23 +0000

    added operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,33 @@ const isLoggedIn = undefined
 
 const isLoggedInInBoolean = Boolean(isLoggedIn)
 
-console.log(isLoggedInInBoolean)
-console.log(typeof isLoggedInInBoolean)
+// console.log(isLoggedInInBoolean)
+// console.log(typeof isLoggedInInBoolean)
+
+//*****************************Operations***************************/
+// Arithmetic Operations -> +, -, *, /, %, **
+
+let num1 = 10
+let num2 = 20
+let sum = num1 + num2
+console.log(sum) // 30
+
+let str1 = "hello"
+let str2 = "world"
+let str3 = str1 + str2 // operator overloading
+console.log(str3)
+
+console.log(num1 - num2)
+console.log(num1 + num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 ** 3)
+console.log(1 + 4 + "2")
+console.log("2" + 4+ 5)
+
+
+// Increment
+// a++, --a
+
+
+
