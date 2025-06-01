fbe24578ac78ae669ad6b402b0b5ab826a3a37a0
yschamp <166626158+yschamp@users.noreply.github.com>
2025-06-01 07:38:00 +0000

    added variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const userName = 'Yuvraj'
+let userMobileNumber = 89808887407
+let userEmail = "yuvraj@google.com"
+var userState = "Gujarat"
+let userPin
+
+// 
+
+/*
+We do not use `var` anymore, as it has function scope and can lead to unexpected behavior.
+*/
+
+
+console.log(userName)
+console.table([userMobileNumber, userEmail, userState, userPin])
