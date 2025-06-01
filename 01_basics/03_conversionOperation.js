89319920c0230b3f0c4268534b71e1b305a5d117
yschamp <166626158+yschamp@users.noreply.github.com>
2025-06-01 08:27:05 +0000

    added conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// const { age = 0 } = req.body
+
+// const age = "33"
+// const age = "abc"
+// const age = true
+// const age = false
+// const age = null
+const age = undefined
+
+const ageInNumber = Number(age)
+
+// console.log(ageInNumber)
+// console.log(typeof ageInNumber)
+
+
+// const isLoggedIn = true
+// const isLoggedIn = false
+// const isLoggedIn = ""
+// const isLoggedIn = "a"
+// const isLoggedIn = 123
+// const isLoggedIn = 0
+// const isLoggedIn = null
+const isLoggedIn = undefined
+
+const isLoggedInInBoolean = Boolean(isLoggedIn)
+
+console.log(isLoggedInInBoolean)
+console.log(typeof isLoggedInInBoolean)
