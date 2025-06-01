6ddf66944380a44e482c63840b6fe09e03dad00b
yschamp <166626158+yschamp@users.noreply.github.com>
2025-06-01 07:54:01 +0000

    added datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+'use strict'
+
+let age = 29
+let name = "Yuvraj"
+let isLoggedIn = true
+let temperature = null
+
+// Data types in JavaScript
+// Number 2 to the power 53
+// BigInt
+// String
+// Boolean
+// undefined -> not yet defined
+/* null -> It is an object as well. empty value, suppose the temperature fetch api is down, the response should be null. You might
+ want to use 0 to indicate that the api is down, however we can't do that since 0 is a valid temperature value */
+// Symbol -> unique identifier
+
+// object
+
+console.log(typeof temperature)
+
+
+// https://tc39.es/ecma262/#sec-intro
+// https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting
